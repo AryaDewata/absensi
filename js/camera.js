e03eab46cf5f6e3a8d88cb1457ad03e9645c7ae0
AryaDewata <madearyadewata@gmail.com>
2023-01-30 10:11:48 +0800

    facing 6

--- js/camera.js
+++ js/camera.js
@@ -3,7 +3,9 @@ let constraints = {
   video: {
     width: window.innerWidth,
     height: window.innerHeight,
-    facingMode: "user",
+    facingMode: {
+      exact: "user",
+    },
   },
 };
 
@@ -26,11 +28,11 @@ init();
 // Facing mode
 const facingBtn = document.querySelector("#facing-btn");
 facingBtn.addEventListener("click", function () {
-  if (constraints.video.facingMode != "user") {
-    constraints.video.facingMode = "user";
+  if (constraints.video.facingMode.exact != "user") {
+    constraints.video.facingMode.exact = "user";
     camera.style.transform = "scaleX(-1)";
   } else {
-    constraints.video.facingMode = "enviroment";
+    constraints.video.facingMode.exact = "enviroment";
     camera.style.transform = "scaleX(1)";
   }
   init();
